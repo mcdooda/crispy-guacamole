5f32a6998571fa369efdb248cdc681887cab17f3
mcdooda <nicolas.dod@gmail.com>
2016-10-02 23:43:17 +0200

    Updated ui tests

--- data/editor/scripts/tests.lua
+++ data/editor/scripts/tests.lua
@@ -10,10 +10,63 @@ local Slider = require 'data/scripts/ui/slider'
 local font = { 'data/misc/fonts/LucidaSansRegular.ttf', 12 }
 
 local root = Widget.getRoot()
+
+do
+	local leftPanel = Widget.makeColumnFlow()
+	leftPanel:setPadding(10)
+	leftPanel:setBackgroundColor(0xFF0000FF)
+	leftPanel:setSizePolicy(Widget.SizePolicy.FIXED_X + Widget.SizePolicy.EXPAND_Y)
+	leftPanel:setSize(100, 0)
+	
+	do
+		local label = Widget.makeText('Editor', unpack(font))
+		label:setPositionPolicy(Widget.PositionPolicy.TOP_LEFT)
+		leftPanel:addChild(label)
+	end
+	
+	do
+		local modes = Widget.makeColumnFlow()
+		modes:setBackgroundColor(0x00FF00FF)
+		modes:setSizePolicy(Widget.SizePolicy.EXPAND_X + Widget.SizePolicy.FIXED_Y)
+		
+		-- entity
+		local entity = Widget.makeText('Entity', unpack(font))
+		modes:addChild(entity)
+		-- tile
+		local tile = Widget.makeText('Tile', unpack(font))
+		modes:addChild(tile)
+		-- prop
+		local prop = Widget.makeText('Prop', unpack(font))
+		modes:addChild(prop)
+		
+		local w, h = prop:getSize()
+		modes:setSize(0, h * 3)
+		
+		leftPanel:addChild(modes)
+	end
+	
+	do
+		local content = Widget.makeColumnFlow()
+		content:setBackgroundColor(0x0000FFFF)
+		content:setSizePolicy(Widget.SizePolicy.EXPAND)
+		
+		for i = 1, 10 do
+			local item = Widget.makeText('Item ' .. i, unpack(font))
+			content:addChild(item)
+		end
+		
+		leftPanel:addChild(content)
+	end
+	
+	root:addChild(leftPanel)
+end
+
 -- top right debug info
 do
 	local debugContainer = Widget.makeColumnFlow()
 	debugContainer:setPositionPolicy(Widget.PositionPolicy.TOP_RIGHT)
+	debugContainer:setSizePolicy(Widget.SizePolicy.FIXED_X + Widget.SizePolicy.EXPAND_Y)
+	debugContainer:setSize(100, 0)
 
 	-- fps
 	do
@@ -58,9 +111,6 @@ do
 				Editor.setEntity(entityName)
 			end)
 		end
-		
-		Editor.setEntityMode()
-		Editor.setEntity(ModData.entities.names[1])
 	end
 	
 	-- tiles
@@ -99,196 +149,41 @@ do
 
 	root:addChild(debugContainer)
 end
+Editor.setEntityMode()
+Editor.setEntity(ModData.entities.names[1])
 
 --[[
--- padding test
 do
-	local red = Widget.makeFixedSize(200, 200)
-	red:setPadding(100, 0, 0, 100)
-	red:setPositionPolicy(Widget.PositionPolicy.CENTER)
-	red:setBackgroundColor(0xFF0000FF)
-	do
-		local blue = Widget.makeFixedSize(50, 50)
-		blue:setBackgroundColor(0x0000FFFF)
-		red:addChild(blue)
-	end
-	do
-		local green = Widget.makeFixedSize(50, 50)
-		green:setPositionPolicy(Widget.PositionPolicy.BOTTOM + Widget.PositionPolicy.CENTER_X)
-		green:setBackgroundColor(0x00FF00FF)
-		red:addChild(green)
-	end
-	root:addChild(red)
-end
-
--- icons toolbar
-do
-	local toolbar = Widget.makeLineFlow()
-	toolbar:setPosition(-10,10)
-	toolbar:setPositionPolicy(Widget.PositionPolicy.BOTTOM_RIGHT)
-
-	local icons = {
-		{
-			'data/editor/interface/brushsizedown.png',
-			function(w)
-				print 'click size -'
-				return true
-			end,
-			function(w)
-				print 'mouse enter size -'
-			end,
-			function(w)
-				print 'mouse leave size -'
-			end
-		},
-		{
-			'data/editor/interface/brushsizeup.png',
-			function(w)
-				print 'click size +'
-				return true
-			end,
-			function(w)
-				print 'mouse enter size +'
-			end,
-			function(w)
-				print 'mouse leave size +'
-			end
-		},
-		{
-			'data/editor/interface/cone.png',
-			function(w)
-				print 'click cone'
-				return true
-			end,
-			function(w)
-				print 'mouse enter cone'
-			end,
-			function(w)
-				print 'mouse leave cone'
-			end
-		},
-		{
-			'data/editor/interface/disc.png',
-			function(w)
-				print 'click disc'
-				return true
-			end,
-			function(w)
-				print 'mouse enter disc'
-			end,
-			function(w)
-				print 'mouse leave disc'
-			end
-		},
-		{
-			'data/editor/interface/pyramid.png',
-			function(w)
-				print 'click pyramid'
-				return true
-			end,
-			function(w)
-				print 'mouse enter pyramid'
-			end,
-			function(w)
-				print 'mouse leave pyramid'
-			end
-		},
-		{
-			'data/editor/interface/square.png',
-			function(w)
-				print 'click square'
-				return true
-			end,
-			function(w)
-				print 'mouse enter square'
-			end,
-			function(w)
-				print 'mouse leave square'
-			end
-		}
+	local a = Widget.makeFixedSize(50, 50)
+	a:setBackgroundColor(0xFF0000FF)
+	a:setPositionPolicy(Widget.PositionPolicy.BOTTOM_LEFT)
+	a:setPadding(2)
+	
+	local positionPolicies = {
+		Widget.PositionPolicy.TOP_LEFT,
+		Widget.PositionPolicy.CENTER_X     + Widget.PositionPolicy.TOP,
+		Widget.PositionPolicy.TOP_RIGHT,
+		Widget.PositionPolicy.RIGHT        + Widget.PositionPolicy.CENTER_Y,
+		Widget.PositionPolicy.BOTTOM_RIGHT,
+		Widget.PositionPolicy.CENTER_X     + Widget.PositionPolicy.BOTTOM,
+		Widget.PositionPolicy.BOTTOM_LEFT,
+		Widget.PositionPolicy.LEFT         + Widget.PositionPolicy.CENTER_Y
 	}
-
-	for _, pair in pairs(icons) do
-		local image, onClick, onMouseEnter, onMouseLeave = unpack(pair)
-		local icon = Widget.makeImage(image)
-		icon:setPosition(math.random(-10, 10), math.random(-10, 10))
-		icon:click(onClick)
-		icon:mouseEnter(onMouseEnter)
-		icon:mouseLeave(onMouseLeave)
-		toolbar:addChild(icon)
+	for i = 1, #positionPolicies do
+		local positionPolicy = positionPolicies[i]
+		local b = Widget.makeFixedSize(5, 5)
+		b:setBackgroundColor(0x00FF00FF)
+		b:setPositionPolicy(positionPolicy)
+		a:addChild(b)
 	end
 	
-	root:addChild(toolbar)
-	
-	--
-	do
-		local icon = Widget.makeFixedSize(64,64)
-		icon:setBackground('data/editor/interface/square.png')
-		icon:setBackgroundRepeat(Widget.BackgroundRepeat.REPEAT)
-		icon:setPositionPolicy(Widget.PositionPolicy.BOTTOM_LEFT)
-		icon:setBackgroundColor(0xFF0000FF)
-		
-		local label = Widget.makeText('LABEL', unpack(font))
-		label:setPositionPolicy(Widget.PositionPolicy.BOTTOM_LEFT)
-		
-		do
-			local child1 = Widget.makeFixedSize(30,30)
-			child1:setPosition(10,10)
-			child1:setPositionPolicy(Widget.PositionPolicy.BOTTOM_LEFT)
-			child1:setBackgroundColor(0xFFFFFFFF)
-			do
-				local child2 = Widget.makeFixedSize(10,10)
-				child2:setPosition(-5,5)
-				child2:setBackgroundColor(0x0000FFFF)
-				child2:setPositionPolicy(Widget.PositionPolicy.TOP_LEFT)
-				child1:addChild(child2)
-			end
-			do
-				local child3 = Widget.makeFixedSize(10,10)
-				child3:setPosition(5,5)
-				child3:setBackgroundColor(0x00FF00FF)
-				child3:setPositionPolicy(Widget.PositionPolicy.TOP_RIGHT)
-				child1:addChild(child3)
-			end
-			do
-				local child4 = Widget.makeFixedSize(10,10)
-				child4:setPosition(-5,-5)
-				child4:setBackgroundColor(0xFF0000FF)
-				child4:setPositionPolicy(Widget.PositionPolicy.BOTTOM_LEFT)
-				child1:addChild(child4)
-			end
-			do
-				local child5 = Widget.makeFixedSize(10,10)
-				child5:setPosition(5,-5)
-				child5:setBackgroundColor(0xFF00FFFF)
-				child5:setPositionPolicy(Widget.PositionPolicy.BOTTOM_RIGHT)
-				child1:addChild(child5)
-			end
-			icon:addChild(child1)
-		end
-		root:addChild(icon)
-		root:addChild(label)
-		Timer.start(0, function()
-			local x, y = Mouse.getPosition()
-			local width, height = icon:getSize()
-			icon:setPosition(x, y - height)
-			label:setPosition(x, y)
-		end)
-	end
-end
-
-do
-	for i = 0, 500, 5 do
-		local slider = Slider:new(100,5)
-		slider:setValue(0.5)
-		slider.container:setPosition(0, -100 - i)
-		root:addChild(slider.container)
-	end
-	for i = 0, 500, 5 do
-		local slider = Slider:new(5,100)
-		slider:setValue(0.5)
-		slider.container:setPosition(100 + i, -100)
-		root:addChild(slider.container)
-	end
+	local w, h = a:getSize()
+	Timer.start(0, function()
+		local x, y = Mouse.getPosition()
+		x = x + 5
+		y = y - h - 5
+		a:setPosition(x, y)
+	end)
+	root:addChild(a)
 end
 ]]

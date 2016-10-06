9a1df4064288d5354e1a51f7e7e0bc3214693b31
mcdooda <nicolas.dod@gmail.com>
2016-10-07 01:36:40 +0200

    Improved editor test ui

--- data/editor/scripts/tests.lua
+++ data/editor/scripts/tests.lua
@@ -13,49 +13,116 @@ local root = Widget.getRoot()
 
 do
 	local leftPanel = Widget.makeColumnFlow()
-	leftPanel:setPadding(10)
-	leftPanel:setBackgroundColor(0xFF0000FF)
-	leftPanel:setSizePolicy(Widget.SizePolicy.FIXED_X + Widget.SizePolicy.EXPAND_Y)
-	leftPanel:setSize(100, 0)
+	leftPanel:setSizePolicy(Widget.SizePolicy.COMPRESS_X + Widget.SizePolicy.EXPAND_Y)
 	
-	do
-		local label = Widget.makeText('Editor', unpack(font))
-		label:setPositionPolicy(Widget.PositionPolicy.TOP_LEFT)
-		leftPanel:addChild(label)
+	local content = Widget.makeColumnFlow()
+	content:setBackgroundColor(0x0000FFFF)
+	content:setSizePolicy(Widget.SizePolicy.COMPRESS_X + Widget.SizePolicy.EXPAND_Y)
+	
+	local contentChildren = {}
+	
+	local function addContent(widget)
+		content:addChild(widget)
+		contentChildren[#contentChildren + 1] = widget
+	end
+	
+	local function clearContent()
+		for i = 1, #contentChildren do
+			contentChildren[i]:removeFromParent()
+			contentChildren[i]:destroy()
+		end
+		contentChildren = {}
 	end
 	
 	do
-		local modes = Widget.makeColumnFlow()
+		local modes = Widget.makeLineFlow()
 		modes:setBackgroundColor(0x00FF00FF)
-		modes:setSizePolicy(Widget.SizePolicy.EXPAND_X + Widget.SizePolicy.COMPRESS_Y)
+		modes:setSizePolicy(Widget.SizePolicy.COMPRESS_X + Widget.SizePolicy.COMPRESS_Y)
+		
+		local function openTilesTab()
+			Editor.setTileMode()
+			Editor.setTile(ModData.tiles.names[1])
+			local title = Widget.makeText('--- Tiles ---', unpack(font))
+			title:setMargin(10, 0, 5, 0)
+			addContent(title)
+			for i = 1, #ModData.tiles.names do
+				local tileName = ModData.tiles.names[i]
+				local label = Widget.makeText(tileName, unpack(font))
+				label:setMargin(0, 0, 0, 7)
+				label:click(function()
+					Editor.setTileMode()
+					Editor.setTile(tileName)
+				end)
+				addContent(label)
+			end
+		end
+		
+		local function openPropsTab()
+			Editor.setPropMode()
+			Editor.setProp(ModData.props.names[1])
+			local title = Widget.makeText('--- Props ---', unpack(font))
+			title:setMargin(10, 0, 5, 0)
+			addContent(title)
+			for i = 1, #ModData.props.names do
+				local propName = ModData.props.names[i]
+				local label = Widget.makeText(propName, unpack(font))
+				label:setMargin(0, 0, 0, 7)
+				label:click(function()
+					Editor.setPropMode()
+					Editor.setProp(propName)
+				end)
+				addContent(label)
+			end
+		end
+		
+		local function openEntitiesTab()
+			Editor.setEntityMode()
+			Editor.setEntity(ModData.entities.names[1])
+			local title = Widget.makeText('--- Entities ---', unpack(font))
+			title:setMargin(10, 0, 5, 0)
+			addContent(title)
+			for i = 1, #ModData.entities.names do
+				local entityName = ModData.entities.names[i]
+				local label = Widget.makeText(entityName, unpack(font))
+				label:setMargin(0, 0, 0, 7)
+				label:click(function()
+					Editor.setEntityMode()
+					Editor.setEntity(entityName)
+				end)
+				addContent(label)
+			end
+		end
 		
 		local tabs = {
-			'buildingstab',
-			'doodadstab',
-			'tilestab',
-			'unitstab',
-			'zonestab'
+			{'tilestab',   openTilesTab},
+			{'doodadstab', openPropsTab},
+			{'unitstab',   openEntitiesTab}
 		}
 		local icons = {}
 		
 		local currentTabIndex
 		local function enableTab(i)
 			currentTabIndex = i
-			icons[i]:setBackground('data/editor/interface/' .. tabs[i] .. '/active.png')
+			local tab = tabs[i][1]
+			local callback = tabs[i][2]
+			icons[i]:setBackground('data/editor/interface/' .. tab .. '/active.png')
+			callback()
 		end
 		local function disableTab(i)
 			assert(currentTabIndex == i)
+			local tab = tabs[i][1]
 			currentTabIndex = nil
-			icons[i]:setBackground('data/editor/interface/' .. tabs[i] .. '/passive.png')
+			icons[i]:setBackground('data/editor/interface/' .. tab .. '/passive.png')
 		end
 		
 		for i = 1, #tabs do
-			local tab = tabs[i]
+			local tab = tabs[i][1]
 			local icon = Widget.makeImage('data/editor/interface/' .. tab .. '/passive.png')
 			icons[i] = icon
 			icon:click(function()
 				if i ~= currentTabIndex then
 					disableTab(currentTabIndex)
+					clearContent()
 					enableTab(i)
 				end
 			end)
@@ -66,112 +133,10 @@ do
 		leftPanel:addChild(modes)
 	end
 	
-	do
-		local content = Widget.makeColumnFlow()
-		content:setBackgroundColor(0x0000FFFF)
-		content:setSizePolicy(Widget.SizePolicy.EXPAND)
-		
-		for i = 1, 10 do
-			local item = Widget.makeText('Item ' .. i, unpack(font))
-			content:addChild(item)
-		end
-		
-		leftPanel:addChild(content)
-	end
+	leftPanel:addChild(content)
 	
 	root:addChild(leftPanel)
 end
-
--- top right debug info
-do
-	local debugContainer = Widget.makeColumnFlow()
-	debugContainer:setPositionPolicy(Widget.PositionPolicy.TOP_RIGHT)
-	debugContainer:setSizePolicy(Widget.SizePolicy.FIXED_X + Widget.SizePolicy.EXPAND_Y)
-	debugContainer:setSize(100, 0)
-
-	-- fps
-	do
-		local label = Widget.makeText('X fps', unpack(font))
-		label:setMargin(10)
-		debugContainer:addChild(label)
-		
-		local getFrameRate = Time.getFrameRate
-		local format = string.format
-		local medianNumFrames = 20
-		local medianFps = {}
-		for j = 1, medianNumFrames do
-			medianFps[j] = getFrameRate()
-		end
-		local i = 1
-		Timer.start(0, function()
-			local fps = getFrameRate()
-			medianFps[i] = nil
-			medianFps[i + medianNumFrames] = fps
-			local displayedFps = 0
-			for j = i + 1, i + medianNumFrames do
-				displayedFps = displayedFps + medianFps[j]
-			end
-			displayedFps = displayedFps / medianNumFrames
-			label:setText(format('%.1f fps', displayedFps))
-			i = i + 1
-		end)
-	end
-	
-	-- entities
-	do
-		local title = Widget.makeText('--- Entities ---', unpack(font))
-		title:setMargin(10, 0, 5, 0)
-		debugContainer:addChild(title)
-		for i = 1, #ModData.entities.names do
-			local entityName = ModData.entities.names[i]
-			local label = Widget.makeText(entityName, unpack(font))
-			label:setMargin(0, 0, 0, 7)
-			debugContainer:addChild(label)
-			label:click(function()
-				Editor.setEntityMode()
-				Editor.setEntity(entityName)
-			end)
-		end
-	end
-	
-	-- tiles
-	do
-		local title = Widget.makeText('--- Tiles ---', unpack(font))
-		title:setMargin(10, 0, 5, 0)
-		debugContainer:addChild(title)
-		for i = 1, #ModData.tiles.names do
-			local tileName = ModData.tiles.names[i]
-			local label = Widget.makeText(tileName, unpack(font))
-			label:setMargin(0, 0, 0, 7)
-			debugContainer:addChild(label)
-			label:click(function()
-				Editor.setTileMode()
-				Editor.setTile(tileName)
-			end)
-		end
-	end
-	
-	-- props
-	do
-		local title = Widget.makeText('--- Props ---', unpack(font))
-		title:setMargin(10, 0, 5, 0)
-		debugContainer:addChild(title)
-		for i = 1, #ModData.props.names do
-			local propName = ModData.props.names[i]
-			local label = Widget.makeText(propName, unpack(font))
-			label:setMargin(0, 0, 0, 7)
-			debugContainer:addChild(label)
-			label:click(function()
-				Editor.setPropMode()
-				Editor.setProp(propName)
-			end)
-		end
-	end
-
-	root:addChild(debugContainer)
-end
-Editor.setEntityMode()
-Editor.setEntity(ModData.entities.names[1])
 
 --[[
 do

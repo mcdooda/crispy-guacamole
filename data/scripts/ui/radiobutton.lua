08e5a9222134424b71dc16f64f010e3a96968b46
Rouillard <jon@rouillard.org>
2016-11-22 22:43:37 +0100

    Renamed checkbox to radiobutton

--- data/scripts/ui/radiobutton.lua
+++ data/scripts/ui/radiobutton.lua
@@ -1,44 +1,51 @@
 
+local font = { 'data/misc/fonts/LucidaSansRegular.ttf', 12 }
 
-local Checkbox = {}
-Checkbox.__index = Checkbox
+local Radiobutton = {}
+Radiobutton.__index = Radiobutton
 
-local function triggerValueChanged(checkbox, value)
-    local callbacks = checkbox.valueChangedCallbacks
+local function triggerValueChanged(radiobutton, value)
+    local callbacks = radiobutton.valueChangedCallbacks
     for i = 1, #callbacks do
-        callbacks[i](checkbox, value)
+        callbacks[i](radiobutton, value)
     end
 end
 
-local function containerClicked(checkbox)
-    checkbox.value = not checkbox.value
+local function containerClicked(radiobutton)
+    radiobutton.value = not radiobutton.value
 
-    checkbox.checkMark:setVisible(checkbox.value)
-    print(checkbox.value)
-    triggerValueChanged(checkbox, value)
+    radiobutton.checkMark:setVisible(radiobutton.value)
+    print(radiobutton.value)
+    triggerValueChanged(radiobutton, value)
     return true
 end
 
-function Checkbox:new(value, width, text, path)
+function Radiobutton:new(value, width, text, path)
     value = value or 0
     width = width or 20
     height = width
-    path = path or 'data/editor/interface/checkbox/'
+    path = path or 'data/editor/interface/radiobutton/'
     assert(width > 0)
 
     local checkMarkContainer = Widget.makeFixedSize(width, height)
     checkMarkContainer:setBackground(path .. 'background.png')
 
-    local checkMark = Widget.makeFixedSize(width - 2, height - 2)
+    local checkMark = Widget.makeFixedSize(width, height)
     checkMark:setBackground(path .. 'foreground.png')
-    checkMark:setPosition(1, -1)
+    checkMark:setPosition(0, 0)
     checkMark:setVisible(value)
     checkMarkContainer:addChild(checkMark)
 
     local container = Widget.makeLineFlow()
     container:setSizePolicy(Widget.SizePolicy.COMPRESS_X + Widget.SizePolicy.COMPRESS_Y)
     container:addChild(checkMarkContainer)
+    container:setBackgroundColor(0xFFFFFFFF)
 
+    if (text) then
+        local label = Widget.makeText(text, unpack(font))
+        label:setMargin(0, 0, 0, 7)
+        container:addChild(label)
+    end
 
     local o = setmetatable({
         container             = container,
@@ -54,15 +61,15 @@ function Checkbox:new(value, width, text, path)
     return o
 end
 
-function Checkbox:getValue()
+function Radiobutton:getValue()
     return self.value
 end
 
-function Checkbox:setValue(value)
+function Radiobutton:setValue(value)
     assert(value == true or value == false)
     self.value = value
     self.checkMark:setVisible(self.value)
     triggerValueChanged(self, value)
 end
 
-return Checkbox
+return Radiobutton

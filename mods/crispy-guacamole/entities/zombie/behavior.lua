235ef06ff6f6df1cf1d92fa338fab0c0f6f7ec57
mcdooda <nicolas.dod@gmail.com>
2016-11-22 23:23:51 +0100

    Added entity:enterStateAsync(), entity:playAnimationAsync()

--- mods/crispy-guacamole/entities/zombie/behavior.lua
+++ mods/crispy-guacamole/entities/zombie/behavior.lua
@@ -1,11 +1,27 @@
 local math = math
 local random = math.random
 
+local function sleep(duration)
+	local getTime = Time.getTime
+	local endTime = getTime() + duration
+	local yield = coroutine.yield
+	while getTime() < endTime do
+		yield()
+	end
+end
+
 local states = {}
 
 function states:init(zombie)
 	zombie:playAnimation 'spawn'
 	zombie:enterState 'wander'
+end
+
+function states:despawn(zombie)
+	zombie:clearPath()
+	zombie:playAnimation 'despawn'
+	sleep(2)
+	zombie:despawn()
 end
 
 function states:idle(zombie)

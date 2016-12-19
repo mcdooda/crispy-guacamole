23708b84c9a6c1fbc7be585ba33796c03d47d428
Rouillard <jon@rouillard.org>
2016-12-19 23:46:54 +0100

    Add radiobutton and radiogroup classes

--- data/scripts/ui/radiobutton.lua
+++ data/scripts/ui/radiobutton.lua
@@ -1,75 +1,87 @@
 
-local font = { 'data/misc/fonts/LucidaSansRegular.ttf', 12 }
+local font = { 'data/misc/fonts/Lato-Medium.ttf', 12 }
 
 local Radiobutton = {}
 Radiobutton.__index = Radiobutton
 
-local function triggerValueChanged(radiobutton, value)
-    local callbacks = radiobutton.valueChangedCallbacks
-    for i = 1, #callbacks do
-        callbacks[i](radiobutton, value)
-    end
-end
+local COLORS = {
+    default = 0x0E0E0EFF,
+    disabled = 0x838383FF,
+}
 
-local function containerClicked(radiobutton)
-    radiobutton.value = not radiobutton.value
-
-    radiobutton.checkMark:setVisible(radiobutton.value)
-    print(radiobutton.value)
-    triggerValueChanged(radiobutton, value)
-    return true
-end
-
-function Radiobutton:new(value, width, text, path)
-    value = value or 0
-    width = width or 20
-    height = width
-    path = path or 'data/editor/interface/radiobutton/'
-    assert(width > 0)
-
-    local checkMarkContainer = Widget.makeFixedSize(width, height)
-    checkMarkContainer:setBackground(path .. 'background.png')
-
-    local checkMark = Widget.makeFixedSize(width, height)
-    checkMark:setBackground(path .. 'foreground.png')
+local STATEPOSITION = {
+    default = {{0,0}, {0,0.5}},
+    disabled =  {{0.5,0}, {0.5,0.5}},
+}
+function Radiobutton:new(value, text, state)
+    text  = text or ""
+    value = value or text
+    color = color or "blue"
+    path  = 'data/editor/interface/controls/'
+    state = state or "default"
+    local checkMarkContainer = Widget.makeFixedSize(20,20)
+    local checkMark = Widget.makeFixedSize(20,20)
     checkMark:setPosition(0, 0)
-    checkMark:setVisible(value)
+    checkMark:setPositionPolicy(Widget.PositionPolicy.CENTER)
+    checkMark:setVisible(false)
     checkMarkContainer:addChild(checkMark)
-
     local container = Widget.makeLineFlow()
-    container:setSizePolicy(Widget.SizePolicy.COMPRESS_X + Widget.SizePolicy.COMPRESS_Y)
+    container:setSizePolicy(Widget.SizePolicy.COMPRESS)
     container:addChild(checkMarkContainer)
-    container:setBackgroundColor(0xFFFFFFFF)
 
-    if (text) then
-        local label = Widget.makeText(text, unpack(font))
-        label:setMargin(0, 0, 0, 7)
-        container:addChild(label)
-    end
+    local label = Widget.makeText(text, unpack(font))
+    label:setMargin(0, 5, 1, 5)
+
+    container:addChild(label)
 
     local o = setmetatable({
         container             = container,
         checkMark             = checkMark,
+        checkMarkContainer    = checkMarkContainer,
+        label                 = label,
         value                 = value,
+        text                  = text,
+        state                 = state,
+        path                  = path,
         valueChangedCallbacks = {}
     }, self)
-
-    local click = function(c) containerClicked(o) end
-    container:click(click)
-    -- container:mouseMove(click)
+    o:setStateColor()
 
     return o
+end
+
+
+function Radiobutton:setStateColor()
+    self.checkMarkContainer:setBackground(self.path .. '/radiobutton.png')
+    self.checkMarkContainer:setBackgroundRepeat(Widget.BackgroundRepeat.REPEAT)
+    self.checkMarkContainer:setBackgroundPosition(STATEPOSITION[self.state][1][1], STATEPOSITION[self.state][1][2])
+    self.checkMark:setBackground(self.path .. '/radiobutton.png')
+    self.checkMark:setBackgroundRepeat(Widget.BackgroundRepeat.REPEAT)
+    self.checkMark:setBackgroundPosition(STATEPOSITION[self.state][2][1], STATEPOSITION[self.state][2][2])
+    self.label:setTextColor(COLORS[self.state])
 end
 
 function Radiobutton:getValue()
     return self.value
 end
+function Radiobutton:getState()
+    return self.state
+end
 
-function Radiobutton:setValue(value)
-    assert(value == true or value == false)
-    self.value = value
-    self.checkMark:setVisible(self.value)
-    triggerValueChanged(self, value)
+function Radiobutton:setState(state)
+    self.state = state
+    self:setStateColor()
+end
+function Radiobutton:toggleState()
+    self.state = self.state == "default" and "disabled" or "default"
+    self:setStateColor()
+end
+function Radiobutton:getText()
+    return self.text
+end
+
+function Radiobutton:setVisible(visible)
+    self.checkMark:setVisible(visible)
 end
 
 return Radiobutton

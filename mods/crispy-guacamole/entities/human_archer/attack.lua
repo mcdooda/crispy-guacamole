c72e9416960309436ffd0ee9d320d35b7755bf87
mcdooda <nicolas.dod@gmail.com>
2016-12-26 20:12:06 +0100

    Fixed arrow heading

--- mods/crispy-guacamole/entities/human_archer/attack.lua
+++ mods/crispy-guacamole/entities/human_archer/attack.lua
@@ -1,3 +1,5 @@
+local atan2 = math.atan2
+
 return {
 	attackRange = 5,
 	attackCooldown = 0.5,
@@ -9,12 +11,15 @@ return {
 		archer:playAnimationAsync 'shoot'
 		
 		Timer.start(0.3, nil, function()
-			if not archer:isValid() then
+			if not archer:isValid() or not target:isValid() then
 				return
 			end
 			
 			local x, y, z = archer:getAttachPoint 'crossbow'
-			local heading = archer:getHeading()
+			local tx, ty, tz = target:getPosition()
+			
+			local heading = atan2(ty - y, tx - x)
+			
 			Entity.spawn('arrow', x, y, z, heading, math.pi / 4)
 		end)
 	end

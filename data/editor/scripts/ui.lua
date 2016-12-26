952813ab6afda8efa41376204ede9fe90f98bb39
mcdooda <nicolas.dod@gmail.com>
2016-12-26 16:31:36 +0100

    Removed blocking and untested ui code

--- data/editor/scripts/ui.lua
+++ data/editor/scripts/ui.lua
@@ -6,7 +6,5 @@ local radiobutton = require 'data/scripts/ui/radiobutton'
 
 local root = Widget.getRoot()
 do
-    local check1 = radiobutton:new(true, 15, 'teeeeest', false)
-    check1.container:setPosition(100,0)
-    root:addChild(check1.container)
+    
 end

8cd4f654276e27918aa3b3c22d8ec3d7b3cb1968
Tom Rouillard <tom@rouillard.org>
2017-04-12 00:01:23 +0200

    add soldier death and attack animation

--- mods/crispy-guacamole/entities/soldier/sprite.lua
+++ mods/crispy-guacamole/entities/soldier/sprite.lua
@@ -1,11 +1,21 @@
 return {
-	origin = {9, 17},
-	size = {3, 1},
+	origin = {16.5, 21},
+	size = {8, 3},
 	animations = {
 		move = {
 			line = 1,
 			numFrames = 3,
 			frameDuration = 0.06
+		},
+		attack = {
+			line = 2,
+			numFrames = 8,
+			frameDuration = 0.08
+		},
+		despawn = {
+			line = 3,
+			numFrames = 7,
+			frameDuration = 0.1
 		}
 	}
 }

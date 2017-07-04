2540e2d2c730ac96d5cd36e563a91c486e9dedeb
mcdooda <nicolas.dod@gmail.com>
2017-07-05 00:59:36 +0200

    Removed sprite component's quadtree cell debug

--- src/game/entity/component/components/sprite/spritecomponent.cpp
+++ src/game/entity/component/components/sprite/spritecomponent.cpp
@@ -240,10 +240,13 @@ void SpriteComponent::debugDraw(debug::DebugDisplay& debugDisplay) const
 {
 	debugDisplay.add2dAABB(m_owner->getAABB());
 
+	// below: debug the owner's sprite's quadtree cell
+	/*
 	const map::Map* map = m_owner->getMap();
 	const map::DisplayManager& mapDisplayManager = map->getDisplayManager();
 	const flat::AABB2& quadtreeCellAABB = mapDisplayManager.getEntityCellAABB(m_owner);
 	debugDisplay.add2dAABB(quadtreeCellAABB, flat::video::Color::RED);
+	*/
 }
 #endif
 

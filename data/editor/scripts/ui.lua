aee5bdc83727e73ab50910051eaa9186fd22382f
Rouillard <jon@rouillard.org>
2017-08-08 21:37:33 +0200

    renamed tests.lua to leftPanel.lua

--- data/editor/scripts/ui.lua
+++ data/editor/scripts/ui.lua
@@ -2,4 +2,4 @@ dump = require 'data/scripts/dump'
 dofile 'data/scripts/debug.lua'
 dofile 'data/scripts/brushposition.lua'
 
-dofile 'data/editor/scripts/tests.lua'
+dofile 'data/editor/scripts/leftPanel.lua'

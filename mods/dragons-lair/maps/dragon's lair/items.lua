64d1139e9c8f9cc4b60c0941257c711e3fd1fb00
Tom Rouillard <tom@rouillard.org>
2018-03-07 00:43:20 +0100

    move items effects in their own file

--- mods/dragons-lair/maps/dragon's lair/items.lua
+++ mods/dragons-lair/maps/dragon's lair/items.lua
@@ -1,38 +1,31 @@
-
-local function add(value, add)
-	return value + add
-end
-
-local function multiply(value, add)
-	return value * add
-end
+local Effects = require 'mods/dragons-lair/maps/dragon\'s lair/effects'
 
 return {
     {
         name = 'Iron ring',
         cost = 20,
-        effect = add,
+        effect = Effects.add,
         factor = 1,
         description = 'Add one damage'
     },
     {
         name = 'Gold ring',
         cost = 60,
-        effect = add,
+        effect = Effects.add,
         factor = 2,
         description = 'Add two damage'
     },
     {
         name = 'Enhanced ring',
         cost = 200,
-        effect = add,
+        effect = Effects.add,
         factor = 5,
         description = 'Add five damage'
     },
     {
         name = 'Magic ring',
         cost = 200,
-        effect = multiply,
+        effect = Effects.multiply,
         factor = 1.2,
         description = 'Multiply your damages by 1.2'
     }

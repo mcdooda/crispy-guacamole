3d27410c8da974b30311d65a62491d64dc1194f0
Tom Rouillard <tom@rouillard.org>
2018-03-07 22:23:22 +0100

    move inn ui code to its ui component

--- mods/dragons-lair/entities/inn/behavior.lua
+++ mods/dragons-lair/entities/inn/behavior.lua
@@ -5,12 +5,7 @@ local quests = {
 }
 
 function states:init(inn)
-	inn:click(function(inn) 			
-		inn:setUiVisible(true)
-	end)
-	inn:deselected(function(inn) 			
-		inn:setUiVisible(false)
-	end)
+
 end
 
 function states:idle(inn)	
--- mods/dragons-lair/entities/inn/ui.lua
+++ mods/dragons-lair/entities/inn/ui.lua
@@ -3,7 +3,13 @@ local Dialog = require 'mods/dragons-lair/ui/entity/dialog'
 local ui = setmetatable({}, { __index = Dialog })
 
 function ui.addedToMap(entity, widget)
-    Dialog.addedToMap(entity, widget)
+	Dialog.addedToMap(entity, widget)
+	entity:selected(function(entity)
+		entity:setUiVisible(true)
+	end)
+	entity:deselected(function(entity) 			
+		entity:setUiVisible(false)
+	end)
 end
 
 return ui

2742b7018f730fe64a4896bf23eb3914ec38947e
Tom Rouillard <tom@rouillard.org>
2018-03-12 22:05:36 +0100

    remove unecessary dragons lair map from crispyguacamole mod

--- mods/crispy-guacamole/maps/dragon's lair/theme.lua
+++
@@ -1,9 +0,0 @@
-local UiSettings = require 'data/scripts/ui/uisettings'
-
-return setmetatable(
-    {
-        BACKGROUND_COLOR = 0xFFFFFFCC,
-        TEXT_COLOR       = 0x111111FF
-    },
-    { __index = UiSettings }
-)

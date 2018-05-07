a598c5cf39df7e231333ddc99367f22d4a4b8ab1
mcdooda <nicolas.dod@gmail.com>
2018-05-07 23:29:04 +0200

    Renamed devil to chaos_devil

---
+++ mods/crispy-guacamole/entities/chaos_devil/collision.graph.lua
@@ -0,0 +1,72 @@
+script {
+  links = {
+    {
+      1,
+      1,
+      2,
+      1,
+    },
+    {
+      3,
+      1,
+      1,
+      1,
+    },
+    {
+      4,
+      1,
+      1,
+      2,
+    },
+    {
+      5,
+      1,
+      1,
+      3,
+    },
+    {
+      6,
+      1,
+      1,
+      4,
+    },
+  },
+  nodes = {
+    {
+      name = 'components/collisioncomponent',
+    },
+    {
+      loadArguments = {
+        {
+          5,
+          'Table',
+        },
+      },
+      name = 'script/scriptoutput',
+    },
+    {
+      loadArguments = {
+        'cylinder',
+      },
+      name = 'string',
+    },
+    {
+      loadArguments = {
+        0.25,
+      },
+      name = 'number',
+    },
+    {
+      loadArguments = {
+        1,
+      },
+      name = 'number',
+    },
+    {
+      loadArguments = {
+        true,
+      },
+      name = 'boolean',
+    },
+  },
+}
---
+++ mods/crispy-guacamole/entities/chaos_devil/sprite.graph.lua
@@ -0,0 +1,162 @@
+script {
+  links = {
+    {
+      1,
+      1,
+      2,
+      1,
+    },
+    {
+      3,
+      1,
+      1,
+      1,
+    },
+    {
+      4,
+      1,
+      1,
+      2,
+    },
+    {
+      5,
+      1,
+      1,
+      3,
+    },
+    {
+      6,
+      1,
+      3,
+      1,
+    },
+    {
+      7,
+      1,
+      3,
+      2,
+    },
+    {
+      8,
+      1,
+      4,
+      1,
+    },
+    {
+      9,
+      1,
+      4,
+      2,
+    },
+    {
+      10,
+      1,
+      5,
+      1,
+    },
+    {
+      11,
+      1,
+      10,
+      1,
+    },
+    {
+      12,
+      1,
+      10,
+      2,
+    },
+    {
+      13,
+      1,
+      10,
+      3,
+    },
+    {
+      14,
+      1,
+      10,
+      4,
+    },
+  },
+  nodes = {
+    {
+      name = 'components/spritecomponent',
+    },
+    {
+      loadArguments = {
+        {
+          5,
+          'Table',
+        },
+      },
+      name = 'script/scriptoutput',
+    },
+    {
+      name = 'vector2',
+    },
+    {
+      name = 'vector2',
+    },
+    {
+      loadArguments = {
+        {
+          5,
+          'Table',
+        },
+      },
+      name = 'script/array',
+    },
+    {
+      loadArguments = {
+        9,
+      },
+      name = 'number',
+    },
+    {
+      loadArguments = {
+        21,
+      },
+      name = 'number',
+    },
+    {
+      loadArguments = {
+        3,
+      },
+      name = 'number',
+    },
+    {
+      loadArguments = {
+        1,
+      },
+      name = 'number',
+    },
+    {
+      name = 'components/spriteanimation',
+    },
+    {
+      loadArguments = {
+        'move',
+      },
+      name = 'string',
+    },
+    {
+      loadArguments = {
+        1,
+      },
+      name = 'number',
+    },
+    {
+      loadArguments = {
+        3,
+      },
+      name = 'number',
+    },
+    {
+      loadArguments = {
+        0.11999999731779,
+      },
+      name = 'number',
+    },
+  },
+}

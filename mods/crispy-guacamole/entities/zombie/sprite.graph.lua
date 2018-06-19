a7f4f8464bc365418f2bfe5a8968a9331ee3dfa6
Tom Rouillard <tom@rouillard.org>
2018-06-19 23:22:39 +0200

    fix zombie and skeleton sprites

--- mods/crispy-guacamole/entities/zombie/sprite.graph.lua
+++ mods/crispy-guacamole/entities/zombie/sprite.graph.lua
@@ -1,177 +1,4 @@
 script {
-  nodes = {
-    {
-      name = 'components/spritecomponent',
-    },
-    {
-      name = 'vector2',
-    },
-    {
-      loadArguments = {
-        10.5,
-      },
-      name = 'number',
-    },
-    {
-      loadArguments = {
-        17,
-      },
-      name = 'number',
-    },
-    {
-      name = 'vector2',
-    },
-    {
-      loadArguments = {
-        10,
-      },
-      name = 'number',
-    },
-    {
-      loadArguments = {
-        4,
-      },
-      name = 'number',
-    },
-    {
-      loadArguments = {
-        10,
-      },
-      name = 'number',
-    },
-    {
-      name = 'components/spriteanimation',
-    },
-    {
-      name = 'components/spriteanimation',
-    },
-    {
-      loadArguments = {
-        1,
-      },
-      name = 'number',
-    },
-    {
-      loadArguments = {
-        0.12,
-      },
-      name = 'number',
-    },
-    {
-      loadArguments = {
-        'attack',
-      },
-      name = 'string',
-    },
-    {
-      loadArguments = {
-        3,
-      },
-      name = 'number',
-    },
-    {
-      loadArguments = {
-        6,
-      },
-      name = 'number',
-    },
-    {
-      loadArguments = {
-        0.12,
-      },
-      name = 'number',
-    },
-    {
-      loadArguments = {
-        {
-          5,
-          'Table',
-        },
-      },
-      name = 'script/scriptoutput',
-    },
-    {
-      loadArguments = {
-        'spawn',
-      },
-      name = 'string',
-    },
-    {
-      loadArguments = {
-        {
-          5,
-          'Table',
-        },
-        {
-          5,
-          'Table',
-        },
-        {
-          5,
-          'Table',
-        },
-        {
-          5,
-          'Table',
-        },
-      },
-      name = 'script/array',
-    },
-    {
-      name = 'components/spriteanimation',
-    },
-    {
-      name = 'components/spriteanimation',
-    },
-    {
-      loadArguments = {
-        'despawn',
-      },
-      name = 'string',
-    },
-    {
-      loadArguments = {
-        'move',
-      },
-      name = 'string',
-    },
-    {
-      loadArguments = {
-        4,
-      },
-      name = 'number',
-    },
-    {
-      loadArguments = {
-        6,
-      },
-      name = 'number',
-    },
-    {
-      loadArguments = {
-        0.12,
-      },
-      name = 'number',
-    },
-    {
-      loadArguments = {
-        2,
-      },
-      name = 'number',
-    },
-    {
-      loadArguments = {
-        4,
-      },
-      name = 'number',
-    },
-    {
-      loadArguments = {
-        0.12,
-      },
-      name = 'number',
-    },
-  },
   links = {
     {
       1,
@@ -340,6 +167,179 @@ script {
       1,
       21,
       4,
+    },
+  },
+  nodes = {
+    {
+      name = 'components/spritecomponent',
+    },
+    {
+      name = 'vector2',
+    },
+    {
+      loadArguments = {
+        10.5,
+      },
+      name = 'number',
+    },
+    {
+      loadArguments = {
+        17,
+      },
+      name = 'number',
+    },
+    {
+      name = 'vector2',
+    },
+    {
+      loadArguments = {
+        11,
+      },
+      name = 'number',
+    },
+    {
+      loadArguments = {
+        4,
+      },
+      name = 'number',
+    },
+    {
+      loadArguments = {
+        11,
+      },
+      name = 'number',
+    },
+    {
+      name = 'components/spriteanimation',
+    },
+    {
+      name = 'components/spriteanimation',
+    },
+    {
+      loadArguments = {
+        1,
+      },
+      name = 'number',
+    },
+    {
+      loadArguments = {
+        0.11999999731779,
+      },
+      name = 'number',
+    },
+    {
+      loadArguments = {
+        'attack',
+      },
+      name = 'string',
+    },
+    {
+      loadArguments = {
+        3,
+      },
+      name = 'number',
+    },
+    {
+      loadArguments = {
+        6,
+      },
+      name = 'number',
+    },
+    {
+      loadArguments = {
+        0.11999999731779,
+      },
+      name = 'number',
+    },
+    {
+      loadArguments = {
+        {
+          5,
+          'Table',
+        },
+      },
+      name = 'script/scriptoutput',
+    },
+    {
+      loadArguments = {
+        'spawn',
+      },
+      name = 'string',
+    },
+    {
+      loadArguments = {
+        {
+          5,
+          'Table',
+        },
+        {
+          5,
+          'Table',
+        },
+        {
+          5,
+          'Table',
+        },
+        {
+          5,
+          'Table',
+        },
+      },
+      name = 'script/array',
+    },
+    {
+      name = 'components/spriteanimation',
+    },
+    {
+      name = 'components/spriteanimation',
+    },
+    {
+      loadArguments = {
+        'despawn',
+      },
+      name = 'string',
+    },
+    {
+      loadArguments = {
+        'move',
+      },
+      name = 'string',
+    },
+    {
+      loadArguments = {
+        4,
+      },
+      name = 'number',
+    },
+    {
+      loadArguments = {
+        6,
+      },
+      name = 'number',
+    },
+    {
+      loadArguments = {
+        0.11999999731779,
+      },
+      name = 'number',
+    },
+    {
+      loadArguments = {
+        2,
+      },
+      name = 'number',
+    },
+    {
+      loadArguments = {
+        4,
+      },
+      name = 'number',
+    },
+    {
+      loadArguments = {
+        0.11999999731779,
+      },
+      name = 'number',
     },
   },
 }

1e223cc0eeeb152648a53ff12b404da3dd6fd95e
mcdooda <nicolas.dod@gmail.com>
2018-07-21 20:18:42 +0200

    Added interaction component information in the entity editor

--- data/entityeditor/scripts/components/interaction.lua
+++ data/entityeditor/scripts/components/interaction.lua
@@ -3,7 +3,7 @@ local UiSettings = require 'data/scripts/ui/uisettings'
 return function(componentDetailsPanel, entityTemplateName, componentTemplate, getEntity)
     do
         local label = Widget.makeText(
-            'Faction: ' .. componentTemplate.faction,
+            'Behavior state: ' .. componentTemplate.behaviorStateName,
             table.unpack(UiSettings.defaultFont)
         )
         label:setMargin(2)

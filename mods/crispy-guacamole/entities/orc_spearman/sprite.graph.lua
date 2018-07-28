0623041250e9641dabd562cfc4fa35c959ad2936
Tom Rouillard <tom@rouillard.org>
2018-07-28 19:22:07 +0200

    change orc_spearman sprite

---
+++ mods/crispy-guacamole/entities/orc_spearman/sprite.graph.lua
@@ -0,0 +1,406 @@
+script {
+  links = {
+    {
+      1,
+      1,
+      2,
+      1,
+    },
+    {
+      3,
+      1,
+      1,
+      1,
+    },
+    {
+      4,
+      1,
+      3,
+      1,
+    },
+    {
+      5,
+      1,
+      3,
+      2,
+    },
+    {
+      6,
+      1,
+      1,
+      2,
+    },
+    {
+      7,
+      1,
+      6,
+      1,
+    },
+    {
+      8,
+      1,
+      6,
+      2,
+    },
+    {
+      9,
+      1,
+      34,
+      1,
+    },
+    {
+      10,
+      1,
+      9,
+      1,
+    },
+    {
+      11,
+      1,
+      9,
+      2,
+    },
+    {
+      12,
+      1,
+      9,
+      3,
+    },
+    {
+      13,
+      1,
+      9,
+      4,
+    },
+    {
+      14,
+      1,
+      30,
+      4,
+    },
+    {
+      15,
+      1,
+      34,
+      2,
+    },
+    {
+      16,
+      1,
+      15,
+      1,
+    },
+    {
+      17,
+      1,
+      15,
+      2,
+    },
+    {
+      18,
+      1,
+      15,
+      3,
+    },
+    {
+      19,
+      1,
+      15,
+      4,
+    },
+    {
+      20,
+      1,
+      34,
+      3,
+    },
+    {
+      21,
+      1,
+      20,
+      1,
+    },
+    {
+      22,
+      1,
+      20,
+      2,
+    },
+    {
+      23,
+      1,
+      20,
+      3,
+    },
+    {
+      24,
+      1,
+      20,
+      4,
+    },
+    {
+      25,
+      1,
+      34,
+      4,
+    },
+    {
+      26,
+      1,
+      25,
+      1,
+    },
+    {
+      27,
+      1,
+      25,
+      2,
+    },
+    {
+      28,
+      1,
+      25,
+      3,
+    },
+    {
+      29,
+      1,
+      25,
+      4,
+    },
+    {
+      30,
+      1,
+      34,
+      5,
+    },
+    {
+      31,
+      1,
+      30,
+      1,
+    },
+    {
+      32,
+      1,
+      30,
+      2,
+    },
+    {
+      33,
+      1,
+      30,
+      3,
+    },
+    {
+      34,
+      1,
+      1,
+      3,
+    },
+  },
+  nodes = {
+    {
+      name = 'components/spritecomponent',
+    },
+    {
+      loadArguments = {
+        {
+          5,
+          'Table',
+        },
+      },
+      name = 'script/scriptoutput',
+    },
+    {
+      name = 'vector2',
+    },
+    {
+      loadArguments = {
+        11,
+      },
+      name = 'number',
+    },
+    {
+      loadArguments = {
+        16,
+      },
+      name = 'number',
+    },
+    {
+      name = 'vector2',
+    },
+    {
+      loadArguments = {
+        6,
+      },
+      name = 'number',
+    },
+    {
+      loadArguments = {
+        5,
+      },
+      name = 'number',
+    },
+    {
+      name = 'components/spriteanimation',
+    },
+    {
+      loadArguments = {
+        'move',
+      },
+      name = 'string',
+    },
+    {
+      loadArguments = {
+        1,
+      },
+      name = 'number',
+    },
+    {
+      loadArguments = {
+        5,
+      },
+      name = 'number',
+    },
+    {
+      loadArguments = {
+        0.090000003576279,
+      },
+      name = 'number',
+    },
+    {
+      loadArguments = {
+        0.090000003576279,
+      },
+      name = 'number',
+    },
+    {
+      name = 'components/spriteanimation',
+    },
+    {
+      loadArguments = {
+        'move2',
+      },
+      name = 'string',
+    },
+    {
+      loadArguments = {
+        2,
+      },
+      name = 'number',
+    },
+    {
+      loadArguments = {
+        5,
+      },
+      name = 'number',
+    },
+    {
+      loadArguments = {
+        0.090000003576279,
+      },
+      name = 'number',
+    },
+    {
+      name = 'components/spriteanimation',
+    },
+    {
+      loadArguments = {
+        'lowerSpear',
+      },
+      name = 'string',
+    },
+    {
+      loadArguments = {
+        3,
+      },
+      name = 'number',
+    },
+    {
+      loadArguments = {
+        4,
+      },
+      name = 'number',
+    },
+    {
+      loadArguments = {
+        0.079999998211861,
+      },
+      name = 'number',
+    },
+    {
+      name = 'components/spriteanimation',
+    },
+    {
+      loadArguments = {
+        'attack',
+      },
+      name = 'string',
+    },
+    {
+      loadArguments = {
+        4,
+      },
+      name = 'number',
+    },
+    {
+      loadArguments = {
+        5,
+      },
+      name = 'number',
+    },
+    {
+      loadArguments = {
+        0.079999998211861,
+      },
+      name = 'number',
+    },
+    {
+      name = 'components/spriteanimation',
+    },
+    {
+      loadArguments = {
+        'death',
+      },
+      name = 'string',
+    },
+    {
+      loadArguments = {
+        5,
+      },
+      name = 'number',
+    },
+    {
+      loadArguments = {
+        6,
+      },
+      name = 'number',
+    },
+    {
+      loadArguments = {
+        {
+          5,
+          'Table',
+        },
+        {
+          5,
+          'Table',
+        },
+        {
+          5,
+          'Table',
+        },
+        {
+          5,
+          'Table',
+        },
+        {
+          5,
+          'Table',
+        },
+      },
+      name = 'script/array',
+    },
+  },
+}

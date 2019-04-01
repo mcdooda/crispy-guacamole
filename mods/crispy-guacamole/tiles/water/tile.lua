3439ca129e2f76d4c37bebed6800a882bac96ce1
Rouillard <jon@rouillard.org>
2019-04-02 00:14:52 +0200

    Updated water and grass tiles

--- mods/crispy-guacamole/tiles/water/tile.lua
+++ mods/crispy-guacamole/tiles/water/tile.lua
@@ -1,23 +1,35 @@
 local probabilities = {
 	100, 5, 5, -- center
-	100, 5, 5, -- top left
-	100, 5, 5, -- top right
-	100        -- top left + right
+	100, -- top left
+	100, -- top right
+	100, 100,
+	100, 100,
+	100,
 }
 
 local centerTiles = {1, 2, 3}
-local topLeftTiles = {4, 5, 6}
-local topRightTiles = {7, 8, 9}
-local topLeftRightTiles = {10}
+local topLeftTiles = {4}
+local topRightTiles = {5}
+local topLeftRightTiles = {6}
+local cornerLeft = {7}
+local cornerRight = {8}
+local cornerBottom = {9}
+local cornerTop = {10}
 
-local function selectTile(tl, tr, br, bl)
-	if (not tl or tl == 'water') and (not tr or tr == 'water') then
+local function selectTile(tl, tr, br, bl, t, r, b, l)
+	if (not tr or tr == 'water') and (not tl or tl == 'water') and (t and t ~= 'water') then
+		return cornerBottom
+	elseif (not tl or tl == 'water') and (not tr or tr == 'water') then
 		return centerTiles
-	elseif (not tl or tl ~= 'water') and (not tr or tr ~= 'water') then
+	elseif (tl and tl ~= 'water') and (tr and tr ~= 'water') then
 		return topLeftRightTiles
-	elseif not tl or tl ~= 'water' then
+	elseif (tl and tl == 'water') and (not tr or tr ~= 'water') and (not t or t == 'water') then
+		return cornerLeft
+	elseif (tr and tr == 'water') and (not tl or tl ~= 'water') and (not t or t == 'water') then
+		return cornerRight
+	elseif tl and tl ~= 'water' then
 		return topLeftTiles
-	elseif not tr or tr ~= 'water' then
+	elseif tr and tr ~= 'water' then
 		return topRightTiles
 	else
 		return topLeftRightTiles
@@ -26,8 +38,8 @@ end
 
 return {
 	probabilities = probabilities,
-	frameDuration = 0.2,
-	numFrames = 3,
 	navigability = Map.Navigability.WATER,
+	frameDuration = 0.15,
+	numFrames = 6,
 	selectTile = selectTile
 }

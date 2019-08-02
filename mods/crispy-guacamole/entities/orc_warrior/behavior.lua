0f3ef9685af614e001b662329b98cdad15954096
mcdooda <nicolas.dod@gmail.com>
2019-08-02 23:06:13 +0200

    Added the orc warrior's rage

--- mods/crispy-guacamole/entities/orc_warrior/behavior.lua
+++ mods/crispy-guacamole/entities/orc_warrior/behavior.lua
@@ -1,1 +1,30 @@
-return flat.graph.script.run 'mods/crispy-guacamole/entities/orc_warrior/behavior'
+local states = {}
+
+function states:init(warrior)
+    warrior:setSpeed(2.5)
+end
+
+function states:followTarget(warrior)
+    warrior:playAnimation('rage', 1, false)
+    warrior:clearPath()
+    warrior:setSpeed(4)
+    while true do
+        local followTarget = warrior:getAttackTarget()
+        if not followTarget or not followTarget:isValid() then
+            warrior:enterState 'init'
+        end
+        local targetPosition = followTarget:getPosition()
+        warrior:moveTo(targetPosition:toVector2())
+    end
+end
+
+function states:onEntityEnteredVisionRange(warrior, target)
+    local currentTarget = warrior:getAttackTarget()
+    if currentTarget and currentTarget:isValid() then
+        return
+    end
+    warrior:setAttackTarget(target)
+    return 'followTarget'
+end
+
+return states

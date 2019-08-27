ef2acec39c0414958707e4c96153cabc8c4b34c3
Rouillard <jon@rouillard.org>
2019-08-27 22:37:31 +0200

    Added human_town_hall entity

---
+++ mods/crispy-guacamole/entities/human_town_hall/sprite.graph.lua
@@ -0,0 +1,163 @@
+return {
+  links = {
+    {
+      1,
+      1,
+      14,
+      1,
+    },
+    {
+      2,
+      1,
+      1,
+      3,
+    },
+    {
+      3,
+      1,
+      1,
+      1,
+    },
+    {
+      4,
+      1,
+      3,
+      1,
+    },
+    {
+      5,
+      1,
+      3,
+      2,
+    },
+    {
+      6,
+      1,
+      1,
+      2,
+    },
+    {
+      7,
+      1,
+      6,
+      1,
+    },
+    {
+      8,
+      1,
+      6,
+      2,
+    },
+    {
+      9,
+      1,
+      2,
+      1,
+    },
+    {
+      10,
+      1,
+      9,
+      1,
+    },
+    {
+      11,
+      1,
+      9,
+      2,
+    },
+    {
+      12,
+      1,
+      9,
+      3,
+    },
+    {
+      13,
+      1,
+      9,
+      4,
+    },
+  },
+  nodeType = 'script',
+  nodes = {
+    {
+      name = 'components/spritecomponent',
+    },
+    {
+      loadArguments = {
+        {
+          5,
+          '',
+        },
+      },
+      name = 'script/array',
+    },
+    {
+      name = 'vector2',
+    },
+    {
+      loadArguments = {
+        37,
+      },
+      name = 'number',
+    },
+    {
+      loadArguments = {
+        71,
+      },
+      name = 'number',
+    },
+    {
+      name = 'vector2',
+    },
+    {
+      loadArguments = {
+        1,
+      },
+      name = 'number',
+    },
+    {
+      loadArguments = {
+        1,
+      },
+      name = 'number',
+    },
+    {
+      name = 'components/spriteanimation',
+    },
+    {
+      loadArguments = {
+        'idle',
+      },
+      name = 'string',
+    },
+    {
+      loadArguments = {
+        1,
+      },
+      name = 'number',
+    },
+    {
+      loadArguments = {
+        1,
+      },
+      name = 'number',
+    },
+    {
+      loadArguments = {
+        1,
+      },
+      name = 'number',
+    },
+    {
+      loadArguments = {
+        {
+          5,
+          'Component',
+        },
+      },
+      name = 'script/scriptoutput',
+    },
+  },
+}

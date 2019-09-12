0d67391f2f77b85e4b2be8ff70d2e1a46489e6aa
Tom <tom@rouillard.org>
2019-09-12 22:55:51 +0200

    Add paladin entity

---
+++ mods/crispy-guacamole/entities/human_paladin/life.graph.lua
@@ -0,0 +1,103 @@
+return {
+  links = {
+    {
+      1,
+      1,
+      3,
+      1,
+    },
+    {
+      1,
+      3,
+      8,
+      1,
+    },
+    {
+      1,
+      4,
+      8,
+      2,
+    },
+    {
+      2,
+      1,
+      1,
+      1,
+    },
+    {
+      5,
+      1,
+      4,
+      3,
+    },
+    {
+      6,
+      1,
+      4,
+      4,
+    },
+    {
+      7,
+      1,
+      4,
+      5,
+    },
+    {
+      8,
+      1,
+      4,
+      1,
+    },
+    {
+      8,
+      2,
+      4,
+      2,
+    },
+  },
+  nodeType = 'script',
+  nodes = {
+    {
+      name = 'components/lifecomponent',
+    },
+    {
+      loadArguments = {
+        100,
+      },
+      name = 'number',
+    },
+    {
+      loadArguments = {
+        {
+          5,
+          'Component',
+        },
+      },
+      name = 'script/scriptoutput',
+    },
+    {
+      name = 'entity/playanimation',
+    },
+    {
+      loadArguments = {
+        'death',
+      },
+      name = 'string',
+    },
+    {
+      loadArguments = {
+        1,
+      },
+      name = 'number',
+    },
+    {
+      loadArguments = {
+        true,
+      },
+      name = 'boolean',
+    },
+    {
+      name = 'entity/clearcycleanimation',
+    },
+  },
+}
---
+++ mods/crispy-guacamole/entities/human_paladin/movement.graph.lua
@@ -0,0 +1,73 @@
+return {
+  links = {
+    {
+      1,
+      1,
+      6,
+      1,
+    },
+    {
+      2,
+      1,
+      1,
+      4,
+    },
+    {
+      3,
+      1,
+      1,
+      3,
+    },
+    {
+      4,
+      1,
+      1,
+      2,
+    },
+    {
+      5,
+      1,
+      1,
+      1,
+    },
+  },
+  nodeType = 'script',
+  nodes = {
+    {
+      name = 'components/movementcomponent',
+    },
+    {
+      loadArguments = {
+        true,
+      },
+      name = 'boolean',
+    },
+    {
+      loadArguments = {
+        9,
+      },
+      name = 'number',
+    },
+    {
+      loadArguments = {
+        2.5,
+      },
+      name = 'number',
+    },
+    {
+      loadArguments = {
+        2.5,
+      },
+      name = 'number',
+    },
+    {
+      loadArguments = {
+        {
+          5,
+          'Component',
+        },
+      },
+      name = 'script/scriptoutput',
+    },
+  },
+}

7951e08d13ffe3cb7d4dfec064910421b4a1a2f2
mcdooda <nicolas.dod@gmail.com>
2020-01-05 22:23:31 +0000

    Improved the fog debug panel

--- data/scripts/fog.lua
+++ data/scripts/fog.lua
@@ -1,17 +1,32 @@
 local Checkbox = require 'data/scripts/ui/checkbox'
+local RadioButton = require 'data/scripts/ui/radiobutton'
+local RadioGroup = require 'data/scripts/ui/radiogroup'
 
 return function(addContainer, makeSeparator, font)
 	local fogDebugContainer = addContainer 'Fog'
 
     do
-        local enableFogCheckbox = Checkbox:new(false, 'Enable Fog')
-        enableFogCheckbox:onValueChanged(function(_, value)
-            if value then
-                Map.setFogType(Map.FogType.HARD)
-            else
-                Map.setFogType(Map.FogType.NONE)
-            end
+        local noFogRadioButton = RadioButton:new(Map.FogType.NONE, 'No fog')
+        local hardFogRadioButton = RadioButton:new(Map.FogType.HARD, 'Hard fog')
+        local softFogRadioButton = RadioButton:new(Map.FogType.SOFT, 'Soft fog')
+        fogDebugContainer:addChild(noFogRadioButton.container)
+        fogDebugContainer:addChild(hardFogRadioButton.container)
+        fogDebugContainer:addChild(softFogRadioButton.container)
+
+        local fogRadioGroup = RadioGroup:new{ noFogRadioButton, hardFogRadioButton, softFogRadioButton }
+        fogRadioGroup:setValue(Map.getFogType())
+        fogRadioGroup.valueChanged:on(function(value)
+            Map.setFogType(value)
         end)
-        fogDebugContainer:addChild(enableFogCheckbox.container)
+        
+        -- monitor external changes
+        -- TODO: plug a callback into the code or something better than this
+        flat.ui.task(function()
+            while true do
+                print(Map.getFogType())
+                fogRadioGroup:setValue(Map.getFogType())
+                coroutine.yield()
+            end
+        end)()
     end
 end

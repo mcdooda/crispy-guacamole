1da0e8aa651fe89eed930a8d73b112a4fc499f21
Tom Rouillard <tom@rouillard.org>
2020-02-09 14:44:53 +0100

    add human barracks

---
+++ mods/crispy-guacamole/entities/human_barracks/sprite.graph.lua
@@ -0,0 +1,163 @@
+return {
+  links = {
+    {
+      1,
+      1,
+      5,
+      1,
+    },
+    {
+      2,
+      1,
+      1,
+      1,
+    },
+    {
+      3,
+      1,
+      2,
+      1,
+    },
+    {
+      4,
+      1,
+      2,
+      2,
+    },
+    {
+      6,
+      1,
+      1,
+      2,
+    },
+    {
+      7,
+      1,
+      6,
+      1,
+    },
+    {
+      8,
+      1,
+      6,
+      2,
+    },
+    {
+      9,
+      1,
+      10,
+      4,
+    },
+    {
+      10,
+      1,
+      14,
+      1,
+    },
+    {
+      11,
+      1,
+      10,
+      1,
+    },
+    {
+      12,
+      1,
+      10,
+      2,
+    },
+    {
+      13,
+      1,
+      10,
+      3,
+    },
+    {
+      14,
+      1,
+      1,
+      3,
+    },
+  },
+  nodeType = 'script',
+  nodes = {
+    {
+      name = 'components/spritecomponent',
+    },
+    {
+      name = 'vector2',
+    },
+    {
+      loadArguments = {
+        32,
+      },
+      name = 'number',
+    },
+    {
+      loadArguments = {
+        57,
+      },
+      name = 'number',
+    },
+    {
+      loadArguments = {
+        {
+          5,
+          'Component',
+        },
+      },
+      name = 'script/scriptoutput',
+    },
+    {
+      name = 'vector2',
+    },
+    {
+      loadArguments = {
+        1,
+      },
+      name = 'number',
+    },
+    {
+      loadArguments = {
+        1,
+      },
+      name = 'number',
+    },
+    {
+      loadArguments = {
+        0,
+      },
+      name = 'number',
+    },
+    {
+      name = 'components/spriteanimation',
+    },
+    {
+      loadArguments = {
+        'stand',
+      },
+      name = 'string',
+    },
+    {
+      loadArguments = {
+        1,
+      },
+      name = 'number',
+    },
+    {
+      loadArguments = {
+        1,
+      },
+      name = 'number',
+    },
+    {
+      loadArguments = {
+        {
+          5,
+          '',
+        },
+      },
+      name = 'script/array',
+    },
+  },
+}

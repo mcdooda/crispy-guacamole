34bec3fca2177a0f47557265270ed519f4244bec
Tom Rouillard <tom@rouillard.org>
2020-02-09 12:29:38 +0100

    update mineral sprite

--- mods/crispy-guacamole/entities/prop_minerals/sprite.graph.lua
+++ mods/crispy-guacamole/entities/prop_minerals/sprite.graph.lua
@@ -1,11 +1,28 @@
 return {
-  nodeType = 'script',
   links = {
     {
       1,
       1,
       2,
       1,
+    },
+    {
+      1,
+      2,
+      24,
+      1,
+    },
+    {
+      1,
+      3,
+      26,
+      2,
+    },
+    {
+      1,
+      3,
+      28,
+      2,
     },
     {
       3,
@@ -49,7 +66,122 @@ return {
       1,
       3,
     },
+    {
+      10,
+      1,
+      9,
+      1,
+    },
+    {
+      11,
+      1,
+      10,
+      1,
+    },
+    {
+      12,
+      1,
+      10,
+      2,
+    },
+    {
+      13,
+      1,
+      10,
+      3,
+    },
+    {
+      14,
+      1,
+      10,
+      4,
+    },
+    {
+      15,
+      1,
+      9,
+      2,
+    },
+    {
+      16,
+      1,
+      15,
+      1,
+    },
+    {
+      17,
+      1,
+      15,
+      2,
+    },
+    {
+      18,
+      1,
+      15,
+      3,
+    },
+    {
+      19,
+      1,
+      15,
+      4,
+    },
+    {
+      20,
+      1,
+      25,
+      1,
+    },
+    {
+      21,
+      1,
+      20,
+      1,
+    },
+    {
+      22,
+      1,
+      20,
+      2,
+    },
+    {
+      23,
+      1,
+      25,
+      2,
+    },
+    {
+      24,
+      1,
+      26,
+      1,
+    },
+    {
+      25,
+      1,
+      24,
+      2,
+    },
+    {
+      26,
+      1,
+      28,
+      1,
+    },
+    {
+      27,
+      1,
+      26,
+      3,
+    },
+    {
+      29,
+      1,
+      28,
+      3,
+    },
   },
+  nodeType = 'script',
   nodes = {
     {
       name = 'components/spritecomponent',
@@ -74,7 +206,7 @@ return {
     },
     {
       loadArguments = {
-        30,
+        25,
       },
       name = 'number',
     },
@@ -89,12 +221,121 @@ return {
     },
     {
       loadArguments = {
+        2,
+      },
+      name = 'number',
+    },
+    {
+      loadArguments = {
+        {
+          5,
+          '',
+        },
+        {
+          5,
+          '',
+        },
+      },
+      name = 'script/array',
+    },
+    {
+      name = 'components/spriteanimation',
+    },
+    {
+      loadArguments = {
+        'first',
+      },
+      name = 'string',
+    },
+    {
+      loadArguments = {
         1,
       },
       name = 'number',
     },
     {
-      name = 'script/array',
+      loadArguments = {
+        1,
+      },
+      name = 'number',
+    },
+    {
+      loadArguments = {
+        0,
+      },
+      name = 'number',
+    },
+    {
+      name = 'components/spriteanimation',
+    },
+    {
+      loadArguments = {
+        'second',
+      },
+      name = 'string',
+    },
+    {
+      loadArguments = {
+        2,
+      },
+      name = 'number',
+    },
+    {
+      loadArguments = {
+        1,
+      },
+      name = 'number',
+    },
+    {
+      loadArguments = {
+        0,
+      },
+      name = 'number',
+    },
+    {
+      name = 'math/randomint',
+    },
+    {
+      loadArguments = {
+        0,
+      },
+      name = 'number',
+    },
+    {
+      loadArguments = {
+        1,
+      },
+      name = 'number',
+    },
+    {
+      loadArguments = {
+        0,
+      },
+      name = 'number',
+    },
+    {
+      name = 'script/condition',
+    },
+    {
+      name = 'math/equal',
+    },
+    {
+      name = 'entity/setcycleanimation',
+    },
+    {
+      loadArguments = {
+        'second',
+      },
+      name = 'string',
+    },
+    {
+      name = 'entity/setcycleanimated',
+    },
+    {
+      loadArguments = {
+        false,
+      },
+      name = 'boolean',
     },
   },
 }

a44c0648628404598110d381cd509ac6f2e43cc3
Rouillard <jon@rouillard.org>
2020-05-31 20:22:53 +0200

    Changed grasses

--- mods/crispy-guacamole/tiles/grass/tile.lua
+++ mods/crispy-guacamole/tiles/grass/tile.lua
@@ -1,5 +1,19 @@
 return {
-	probabilities = {1000, 10, 10, 10, 10, 10, 1, 1, 1}, -- 7 tile variants
+	probabilities = {
+		1000,
+		10,
+		10,
+		10,
+		10,
+		10,
+		1,
+		1,
+		1,
+		70,
+		70,
+		70,
+		70
+	}, -- 7 tile variants
 	frameDuration = 0.15,
 	numFrames = 4
 }

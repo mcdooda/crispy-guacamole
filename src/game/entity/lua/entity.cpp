abed9bb939f4039048a0a70c9af3c92a57cc1038
mcdooda <nicolas.dod@gmail.com>
2020-12-31 12:31:10 +0000

    Fixed entity:setPosition2d()

--- src/game/entity/lua/entity.cpp
+++ src/game/entity/lua/entity.cpp
@@ -411,7 +411,7 @@ int l_Entity_setPosition2d(lua_State* L)
 
 	flat::Vector3 position = flat::Vector3(position2d, 0.f);
 	const map::TileIndex tileIndex = baseMapState.getMap().getTileIndex(position.x, position.y);
-	if (map::isValidTile(tileIndex))
+	if (!map::isValidTile(tileIndex))
 	{
 		luaL_error(L, "Trying to set an entity's position outside the map (%f, %f)", position.x, position.y);
 	}
